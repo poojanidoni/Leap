1032a9df0ecf0c365d3fd3f1d57d0b6ddddf9335
poojanidoni <poojanidonixworksz@gmail.com>
2021-12-27 23:38:52 +0530

    Leap

---
+++ Leap.java
@@ -0,0 +1,17 @@
+class Leap{
+	public static void main(String[] argu){
+	int year;
+	if(year % 4==0)
+	{
+	if (year % 100!=0)
+	{
+	if (year%400==0))
+	{
+	System.out.println("it is a leap year");
+	else
+	System.out.println("it is not leap year");
+}
+}
+}
+}
+}
